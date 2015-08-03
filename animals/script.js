74e46e19307f859a286e333d87ee4341ab2e7690
Henry Rosvick <hhrosvick@gmail.com>
2015-08-02 22:19:03 -0600

    Animal fixes and improvements.

--- animals/script.js
+++ animals/script.js
@@ -1,47 +1,26 @@
-
-
-
 $(function(){
-
   $('#randomize').click(function(){
-
-
     // Mostly logic to separate out the bad animals... i mean meat.
     var raw_meat = $('#animal_list').val().split(/\n/);
     var tested_meat = [];
     for (var i=0; i < raw_meat.length; i++) {
-      if (/\S/.test(raw_meat[i])) {
-        tested_meat.push($.trim(raw_meat[i]));
-      }
-    }
-
+      if (/\S/.test(raw_meat[i])) {tested_meat.push($.trim(raw_meat[i]));}}
     var trimmed_meat = [];
     for (var i=0; i < tested_meat.length; i++) {
       var meat_parts = tested_meat[i].split(',');
       if(meat_parts.length >= 3){
-        trimmed_meat.push({
-            name: $.trim(meat_parts[0]),
-            prefix: $.trim(meat_parts[1]),
-            suffix: $.trim(meat_parts[2])
-          });
-      }
-    }
-    console.log(trimmed_meat);
-
+        trimmed_meat.push({name: $.trim(meat_parts[0]),
+          prefix: $.trim(meat_parts[1]),
+          suffix: $.trim(meat_parts[2])});}}
     // If there is only one (or no!) animal.... just give up.
     if(trimmed_meat.length <= 1) return;
-
     // Choosing the meat... i mean animals.
     var left_animal_choice = Math.floor(Math.random() * (trimmed_meat.length));
     var right_animal_choice = left_animal_choice;
-    while(right_animal_choice == left_animal_choice){
-      right_animal_choice = Math.floor(Math.random() * (trimmed_meat.length));
-    }
-
+    while(right_animal_choice == left_animal_choice){right_animal_choice = Math.floor(Math.random() * (trimmed_meat.length));}
+    // Putting them on display.
     $('#left_animal').html(trimmed_meat[left_animal_choice].name);
     $('#right_animal').html(trimmed_meat[right_animal_choice].name);
     $('#randimal').html(trimmed_meat[left_animal_choice].prefix + '' + trimmed_meat[right_animal_choice].suffix);
-
   });
-
 });
